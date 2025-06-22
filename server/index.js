bdfd1a4560251de40f24f474b12b7c95d1ed3d8e
22pa1a05d4 <22pa1a05d4@vishnu.edu.in>
2025-06-22 18:39:00 +0530

    final

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ const notifRoutes = require('./routes/notifications');
 const messageRoutes = require('./routes/messages');
 const searchRoutes =require('./routes/search');
 const User = require('./models/User')
+const PORT = process.env.PORT || 5000;
+const bcrypt = require('bcryptjs');
 
 const app = express();
 app.use(cors());
