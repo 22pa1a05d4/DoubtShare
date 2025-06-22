9b020df6808bce297982796f20450cb3fde2a78b
22pa1a05d4 <22pa1a05d4@vishnu.edu.in>
2025-06-22 19:00:33 +0530

    changed

--- server/index.js
+++ server/index.js
@@ -30,11 +30,11 @@ app.use(
   express.static(path.join(__dirname, 'uploads'))
 );
 
-mongoose.connect('mongodb://127.0.0.1:27017/studentDB', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
